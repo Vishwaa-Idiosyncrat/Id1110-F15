8a531cacf85b425f6207c6164639566acec18011
Vishwaa-Idiosyncrat <vishwaaps3@gmail.com>
2023-05-23 19:28:37 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,22 @@
+import nltk
+import numpy as np
+import random
+import string
+import bs4 as bs
+import urllib.request
+import re
 
+
+get_link = urllib.request.urlopen('https://en.wikipedia.org/wiki/Data_science')
+get_link = get_link.read()
+
+data = bs.BeautifulSoup(get_link, 'lxml')
+data_paragraphs = data.find_all('p')
+
+data_text = ''
+for para in data_paragraphs:
+    data_text += para.text
+
+data_text = data_text.lower()
+
+print(data_text)
