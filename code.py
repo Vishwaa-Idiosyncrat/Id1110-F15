c8a7d52e3fc012e7fb78cb9067390b91b121fa6c
Vishwaa-Idiosyncrat <133481192+Vishwaa-Idiosyncrat@users.noreply.github.com>
2023-05-23 09:53:40 +0530

    Update code.py

--- code.py
+++ code.py
@@ -2,3 +2,9 @@ import nltk
 import numpy as np
 import random
 import string
+import bs4 as bs
+import urllib.request
+import re
+link = urllib.request.urlopen('https://en.wikipedia.org/wiki/Data_science')
+link = link.read()
+data = bs.BeautifulSoup(link,'lxml')
