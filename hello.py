629c708e2cebf8e066eaff3a235511c6f0d1d972
Your Name <vishwaaps3@gmail.com>
2023-05-24 10:53:07 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,13 +1,11 @@
 import nltk
+nltk.download('punkt')
 import numpy as np
 import random
 import string
 import bs4 as bs
 import urllib.request
 import re
-
-
-
 get_link = urllib.request.urlopen('https://en.wikipedia.org/wiki/Data_science')
 get_link = get_link.read()
 
@@ -19,4 +17,10 @@ for para in data_paragraphs:
     data_text += para.text
 
 data_text = data_text.lower()
-print(data_paragraphs)
+
+data_text = re.sub(r'\[[0-9]*\]', ' ',data_text)
+data_text = re.sub(r'\s+',' ',data_text)
+print(data_text)
+sen = nltk.sent_tokenize(data_text)
+words = nltk.word_tokenize(data_text)
+print(sen)
