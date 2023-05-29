9126e3bfd14dd9fe73ebe24f39dcf45bb77b3ff8
Your Name <vishwaaps3@gmail.com>
2023-05-29 11:55:56 +0530

    Update hello5.py

--- hello5.py
+++ hello5.py
@@ -6,6 +6,17 @@ import bs4 as bs
 import urllib.request
 import re
 
+
+inputs = ("hey","hello","good morning", "good afternoon","good evening","morning","evening","afternoon","hi", "whatsup")
+outputs = ["hey","Good Morning", "Good Afternoon","Good Evening"," It’s nice to meet you","Pleased to meet you"," How have you been?"," How do you do?","Hey","Hi"," How’s it going?"]
+
+def greeting_response(greeting):
+    for token in greeting.split():
+        if token.lower() in inputs:
+            return random.choice(outputs)
+start1= True
+print("Hello F-15, I am your personal assistant")
+
 while True:
     get_link = urllib.request.urlopen("https://en.wikipedia.org/wiki/Special:Random")
     soup = bs.BeautifulSoup(get_link, "html.parser")
@@ -44,13 +55,7 @@ pr = dict((ord(punctuation),None) for punctuation in string.punctuation)
 def processed_text(document):
     return lemmatization(nltk.word_tokenize(document.lower().translate(pr)))
 
-inputs = ("hey","hello","good morning", "good afternoon","good evening","morning","evening","afternoon","hi", "whatsup")
-outputs = ["hey","Good Morning", "Good Afternoon","Good Evening"," It’s nice to meet you","Pleased to meet you"," How have you been?"," How do you do?","Hey","Hi"," How’s it going?"]
 
-def greeting_response(greeting):
-    for token in greeting.split():
-        if token.lower() in inputs:
-            return random.choice(outputs)
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
@@ -62,11 +67,11 @@ def generate_response(user_input):
     word_vectorizer = TfidfVectorizer(tokenizer=processed_text, stop_words='english')
     word_vectors = word_vectorizer.fit_transform(sen)
     similar_vector_values = cosine_similarity(word_vectors[-1],word_vectors)
-    similar_sentence_numbers = similar_vector_values.argsort()[0][-2]
+    similar_sentence_numbers = similar_vector_values.argsort()[0][-1]
 
     matched_vector = similar_vector_values.flatten()
     matched_vector.sort()
-    vector_matched = matched_vector[-2]
+    vector_matched = matched_vector[-1]
 
     if similar_sentence_numbers == -1:
         bot_response = bot_response +"I am sorry, I did not understand"
@@ -78,8 +83,7 @@ def generate_response(user_input):
         bot_response = bot_response + sen[similar_sentence_numbers]
         return bot_response
     
-start = True
-print("Hello F-15, I am your personal assistant")
+start=True
 print(f"Question me about {title} ")
 while start == True:
     human = input()
@@ -92,9 +96,12 @@ while start == True:
             if greeting_response(human) != None:
                 print("F-15 Bot: "+ greeting_response(human))
             else:
-                print("F-15 Bot: ", end='')
-                print(generate_response(human))
-                sen.remove(human)
+                bot_response = generate_response(human)
+                if bot_response == human:
+                    print("F-15 Bot: I am sorry, I did not understand")
+                else:
+                    print("F-15 Bot: " + bot_response)
+                    sen.remove(human)
     else:
         start = False
         print("F-15 Bot wishes you all the best")
