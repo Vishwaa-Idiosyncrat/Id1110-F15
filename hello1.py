c2bc0b96e5196a517f2bb3ef2c11a880cb7a8fc4
chiruhask <134256339+chiruhask@users.noreply.github.com>
2023-06-03 23:32:21 +0530

    Update hello1.py

--- hello1.py
+++ hello1.py
@@ -87,3 +87,13 @@ while start:
     else:
         start = False
         print("F-15 Bot wishes you All The Best")
+        
+ import urllib.request
+from bs4 import BeautifulSoup
+
+get_link = urllib.request.urlopen("https://en.wikipedia.org/wiki/Special:Random")
+get_link = get_link.read()
+soup = BeautifulSoup(get_link, "html.parser")
+title = soup.find(class_="firstHeading").text
+print(title)
+       
