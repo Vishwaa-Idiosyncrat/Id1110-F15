221bacf4b6322176f90eacab170f76d4d23f6cad
Gaurav nagar <126567222+Gaurav1981nagar@users.noreply.github.com>
2023-06-04 11:28:06 +0530

    Update gradio_website.py

--- gradio_website.py
+++ gradio_website.py
@@ -96,7 +96,7 @@ def generate_response(user_input):
 
 def process_text():
     user_input=input_textbox.read("r")
-    output_text =get_generate_response(user_input)
+    output_text ="F-15:"+ generate_response(user_input)
   
     if greeting_response(human) != None:
         print("F-15 Bot: " + greeting_response(human))
