7d0efd4479c79c4f496dfaa3610662e91d4e2046
Gaurav nagar <126567222+Gaurav1981nagar@users.noreply.github.com>
2023-06-04 11:21:01 +0530

    Update gradio_website.py

--- gradio_website.py
+++ gradio_website.py
@@ -14,7 +14,7 @@ input_textbox=gr.inputs.Textbox()
 output_textbox=gr.outputs.Textbox()
 
 
-
+start = True
 print("Hello F-15, I am your personal assistant")
 # Get a random Wikipedia article
 get_link = urllib.request.urlopen("https://en.wikipedia.org/wiki/Special:Random")
