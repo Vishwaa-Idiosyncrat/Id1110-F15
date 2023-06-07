0732a8d6366a4816861df75a842e9c2665aa8a42
Gaurav nagar <126567222+Gaurav1981nagar@users.noreply.github.com>
2023-06-07 11:41:23 +0530

    Update gradio_website.py

--- gradio_website.py
+++ gradio_website.py
@@ -106,7 +106,7 @@ def process_text(user_input):
     if greeting_response(human) != None:
         return "F-15 Bot: " + greeting_response(user_input)
 
-    return  output_text
+    return  output_textbox.read
 
 interface=gr.Interface(fn=process_text,inputs=input_textbox,outputs=output_textbox,title="F-15 assistant")
 
